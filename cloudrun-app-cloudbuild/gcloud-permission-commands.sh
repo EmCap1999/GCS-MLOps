74d9992fd955502e52fc40aa620473369b9b90fb
Emmanuël Caputo <caputoemmanuel1999@gmail.com>
2025-01-08 00:15:44 +0100

    only necessary data

--- cloudrun-app-cloudbuild/gcloud-permission-commands.sh
+++
@@ -1,8 +0,0 @@
-# Assign Service account user role to the service account 
-gcloud projects add-iam-policy-binding gcp-udemy-project-447111 \
---member=serviceAccount:323086875037@cloudbuild.gserviceaccount.com --role=roles/iam.serviceAccountUser
-
-
-# Assign Cloud Run role to the service account 
-gcloud projects add-iam-policy-binding gcp-udemy-project-447111 \
-  --member=serviceAccount:323086875037@cloudbuild.gserviceaccount.com --role=roles/run.admin
